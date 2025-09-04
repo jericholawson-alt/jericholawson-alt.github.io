a34afe80c3dca00f43a99c3a8d2a22ab85b2e3c8
jericho.lawson <jboloslawson@gmail.com>
2025-09-04 19:42:26 -0400

    updates

--- tests/testthat/test-1-process-manual.R
+++
@@ -1,9 +0,0 @@
-test_that("process manual", {
-  manuals <- "test.texi"
-  expect_null(
-    process_manual(manuals, .quicktest = TRUE)
-  )
-  expect_null(
-    process_manual(manuals, .quicktest = TRUE, .make_info = TRUE)
-  )
-})
